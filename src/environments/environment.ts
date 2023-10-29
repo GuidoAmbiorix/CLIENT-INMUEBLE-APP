b74d83943023d1293969a324f4df2aac77e64bd8
GuidoAmbiorix <guidoambiorix@gmail.com>
2023-10-29 08:26:05 -0400

    Cambios en environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production:false,
+   firebase:{
+    config : {
+      apiKey: "AIzaSyC7O57SQGiAqVZPOrxJRvNSK5CvlVYe0zY",
+      authDomain: "edificacion-app-657e6.firebaseapp.com",
+      projectId: "edificacion-app-657e6",
+      storageBucket: "edificacion-app-657e6.appspot.com",
+      messagingSenderId: "439181533277",
+      appId: "1:439181533277:web:cbdf58b11bd4d915ec2db4"
+    }
+  }
+}
