e9bac931d63f6096f64001427e8469528e66445a
GuidoA_Amparo <GuidoA_Amparo@jce.do>
2023-11-03 17:19:07 -0400

    --Se empezo a configurar firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
   name: 'prod',
+  firebase: {
+    firebaseConfig: {
+      apiKey: 'AIzaSyC7O57SQGiAqVZPOrxJRvNSK5CvlVYe0zY',
+      authDomain: 'edificacion-app-657e6.firebaseapp.com',
+      projectId: 'edificacion-app-657e6',
+      storageBucket: 'edificacion-app-657e6.appspot.com',
+      messagingSenderId: '439181533277',
+      appId: '1:439181533277:web:cbdf58b11bd4d915ec2db4',
+    },
+  },
 };
