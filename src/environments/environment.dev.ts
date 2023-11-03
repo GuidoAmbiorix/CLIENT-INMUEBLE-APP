7cde458512b12c41b5f39aecb0fcce78d39efbb8
GuidoA_Amparo <GuidoA_Amparo@jce.do>
2023-11-03 17:25:13 -0400

    --

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,20 +1,8 @@
 export const environment = {
   production: false,
   name: 'dev',
-<<<<<<< HEAD
-  firebase:{
-    config : {
-      apiKey: "AIzaSyC7O57SQGiAqVZPOrxJRvNSK5CvlVYe0zY",
-      authDomain: "edificacion-app-657e6.firebaseapp.com",
-      projectId: "edificacion-app-657e6",
-      storageBucket: "edificacion-app-657e6.appspot.com",
-      messagingSenderId: "439181533277",
-      appId: "1:439181533277:web:cbdf58b11bd4d915ec2db4"
-    }
-  }
-=======
   firebase: {
-    firebaseConfig: {
+    config: {
       apiKey: 'AIzaSyC7O57SQGiAqVZPOrxJRvNSK5CvlVYe0zY',
       authDomain: 'edificacion-app-657e6.firebaseapp.com',
       projectId: 'edificacion-app-657e6',
@@ -23,5 +11,4 @@ export const environment = {
       appId: '1:439181533277:web:cbdf58b11bd4d915ec2db4',
     },
   },
->>>>>>> origin/main
 };
